7aefa2d09f09d40a122a3e1412ddbf8c6e820dd7
kmjak <tymkoron1221@icloud.com>
2024-10-07 03:05:34 +0900

    インデントがおかしいところを修正した

--- src/app/components/Authed.tsx
+++ src/app/components/Authed.tsx
@@ -14,23 +14,23 @@ export const AuthedComponent = ({
 } : AuthedComponentProps ) => {
   return (
     <div className="flex flex-col items-center">
-    <h1 className="text-3xl font-semibold text-center">You are logged in as {logindata?.name}</h1>
-    <p>
-      <button
-        onClick={() => router.push("/verified/")}
-        className="bg-blue-500 text-white rounded p-2 m-2 w-64"
-      >
-        login
-      </button>
-    </p>
-    <p>
-      <button
-        onClick={handleLogout}
-        className="bg-red-500 text-white rounded p-2 m-2 w-64"
-      >
-        logout
-      </button>
-    </p>
-  </div>
+      <h1 className="text-3xl font-semibold text-center">You are logged in as {logindata?.name}</h1>
+      <p>
+        <button
+          onClick={() => router.push("/verified/")}
+          className="bg-blue-500 text-white rounded p-2 m-2 w-64"
+        >
+          login
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 text-white rounded p-2 m-2 w-64"
+        >
+          logout
+        </button>
+      </p>
+    </div>
   );
 }
